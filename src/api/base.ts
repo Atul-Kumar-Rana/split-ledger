468922fc0f5254a2c2455bae40884c14ede1f79b
Atul <atul5465king@gmail.com>
2025-12-04 03:06:47 +0530

    updated version

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE || 'http://localhost:8080';
+export const BASE_URL = import.meta.env.VITE_API_BASE || 'http://ec2-3-110-153-176.ap-south-1.compute.amazonaws.com:8080';
